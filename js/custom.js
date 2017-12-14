fe392130b510b92e9932da2625ae6e9e7e7abb50
Bijay Upreti <bjupreti123@gmail.com>
2017-12-14 19:22:42 +0545

    Preloader fixed positioning conflict resolved

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,14 @@
 $(function() {
 	"use strict";
 	// Preloader fades out when the document is ready
-	$("#spinner").fadeOut(300, function() {
-		$("#overlay-left").animate({left: "-50vw"}, 300);
-		$("#overlay-right").animate({right: "-50vw"}, 300);
+	var spinner = $("#spinner");
+	$(spinner).fadeOut(300, function() {
+		// $("#overlay-left").animate({left: "-50vw"}, 300);
+		// $("#overlay-right").animate({right: "-50vw"}, 300);
+		spinner.parent().find("#overlay-left").animate({left: "-50vw"}, 300);
+		spinner.parent().find("#overlay-right").animate({right: "-50vw"}, 300, function() {
+			$(spinner.parent()).fadeOut(10);
+		});
+		// spinner.parent().addClass("position-relative");
 	});
 });
